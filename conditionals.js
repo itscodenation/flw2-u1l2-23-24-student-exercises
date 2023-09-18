620b64e0035631da6bb36fd184648a9d7da9ecc1
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-18 17:34:24 -0400

    Add conditionals exercise

---
+++ conditionals.js
@@ -0,0 +1,51 @@
+//selector variables
+const oven = document.querySelector("#oven");
+const cookie = document.querySelector("#cookie");
+const factory = document.querySelector("#factory");
+let displayCookies = document.querySelector("#total-cookies");
+let displayOvens = document.querySelector("#total-ovens");
+
+// 1. Declare three variables.
+// - `totalCookies`
+// - `ovens`
+// - `factories`
+// -  Set each one to a value of 0
+
+cookie.addEventListener("click", function () {
+// 2. Reassign the value of totalCookie to be itself plus 1. 
+// - See if you can find a shorthand way to do this with a Google search.
+  
+
+// 3. Uncomment the line below. 
+// - Check to see that the amount of cookies increases when you click the image and that the number displays correctly.
+// displayCookies.innerHTML = totalCookies;
+});
+
+oven.addEventListener("click", function () {
+// 4. Create an if/else statement. 
+// - Anytime the totalCookies is greater than or equal to 30, these four things happen:
+//      - Reassign the totalCookies variable to itself minus 30
+//      - Update the displayCookies innerHTML to equal totalCookies
+//      - Reassign the ovens variable to itself plus 1
+//      - Update the displayOvens innerHTML to equal ovens
+// - Else, an alert will pop up that says "You need 30 cookies to buy an oven!" (or something that gets that message across)
+
+
+
+});
+
+// 5. The factory is locked! Based on how the oven section works, see if you can get a factory into the game.
+// - You will need to update the HTML to display the numbers of factories. 
+// - Don't forget to save it as a variable at the top with your other selectors! */
+
+
+
+//UPDATES TOTALCOOKIES EVERY SECOND (1000 MILLISECONDS), ONCE AN OVEN IS ACCESSED
+//UNCOMMENT TO USE
+window.setInterval(function () {
+  /* task 7: this line below needs updating if you add a 
+  factory. How could we change it?*/
+  //   totalCookies = totalCookies + 12 * ovens;
+  //   displayCookies.innerHTML = totalCookies;
+  // }, 1000);
+});
---
+++ variables.js
@@ -0,0 +1,30 @@
+//tile divs
+let tile1 = document.querySelector(".tile1");
+let tile2 = document.querySelector(".tile2");
+let tile3 = document.querySelector(".tile3");
+let tile4 = document.querySelector(".tile4");
+
+// 1. Declare a variable called fullName and assign a string with your name or a favorite celebrity.
+
+// 2. Declare a variable called favNum and assign your favorite number as the value.
+
+// 3. Declare a variable called coolImg and assign a string of an image address. Pick an image you enjoy!
+
+// 4. Declare a variable called secondFavNum and assign a number as the value.
+
+// 5. Declare a variable called sumOfNums and assign a value of favNum added with secondFavNum.
+
+
+
+
+
+
+// 6.  Once 1-5 are complete, uncomment the code below.
+/*
+tile1.innerHTML = fullName;
+tile2.innerHTML = favNum;
+let image = document.createElement("img");
+image.src = coolImg;
+tile3.appendChild(image);
+tile4.innerHTML = sumOfNums;
+*/
